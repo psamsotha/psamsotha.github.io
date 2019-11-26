6166ac048dcda7b553e431d42d199f254c571f0f
Paul Samsotha <paulsamsotha.dev@gmail.com>
2019-11-26 11:06:51 -0800

    Update 2019-11-26T19:06:50.340Z

---
+++ js/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+(function (doc) {
+  $(doc).foundation();
+
+  /**
+   * Menu toggle on small screens.
+   */
+  $('.topbar-nav-label').on('click', function (e) {
+    e.preventDefault();
+    $('.topbar-nav').toggleClass('collapse');
+    $(this).toggleClass('topbar-nav-open');
+  });
+
+  /**
+   * Set active nav item
+   */
+   var page = $('.site-header').attr('data-page');
+   $('.topbar-nav-item').each(function (index, el) {
+     var el = $(el);
+     if (el.attr('title') === page) {
+       el.addClass('active');
+     }
+   });
+
+   /**
+    * Make link clicks open new tab
+    */
+   function handleLinks() {
+     var host = location.hostname;
+     $('a').each(function (index, el) {
+       if (el.hostname !== host && el.hostname !== '') {
+         $(el).attr('target', '_blank');
+       }
+     })
+   }
+   handleLinks();
+})(document);
+
